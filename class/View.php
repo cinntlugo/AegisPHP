8c1bb8ef7e176caf591489ddfa511884687ba587
Diego Islas Ocampo <hyuchia@gmail.com>
2016-03-16 16:49:41 -0600

    Rebuild templating system

--- class/View.php
+++ class/View.php
@@ -10,21 +10,18 @@
 
 
 		private $view;
-		private $template;
 		private $data;
-		private $type;
 		private $meta;
+		private $viewContent;
 
-		function __construct($view, $data = null, $meta = null, $template = "default.php", $type = null){
+		function __construct($data = null, $meta = null, $view = "default.php"){
 			$this -> view = $view;
-			$this -> template = $template;
 			$this -> data = $data;
-			$this -> type = $type;
 			$this -> meta = $meta;
 	    }
 
 	    public function getView(){
-		    return $this -> view;
+		    return $this -> viewContent;
 	    }
 
 	    public function isCompilable(){
@@ -32,29 +29,82 @@
 	    }
 
 		public function compile(){
-
-			$viewContent = file_get_contents("views/$this->view");
-
-			if($this -> template != null){
-				$templateContent = file_get_contents("templates/$this->template");
-				if($this -> meta != null){
-					foreach($this -> meta as $key => $value){
-						$templateContent = str_replace("{{".$key."}}", $value, $templateContent);
-					}
-				}
-				$mergedContent = str_replace("{{content}}", $viewContent, $templateContent);
-			}else{
-				$mergedContent = $viewContent;
-			}
-
-
-			if($this -> data != null){
-				foreach($this -> data as $key => $value){
-					$mergedContent = str_replace("{{".$key."}}", $value, $mergedContent);
-				}
-			}
-			return $mergedContent;
+			$this -> setViewInformation();
+			$this -> compileTemplates();
+			return $this -> viewContent;
 		}
+
+		private function setViewInformation(){
+			$this -> viewContent = file_get_contents("views/$this->view");
+			if($this -> meta != null){
+				foreach($this -> meta as $key => $value){
+					$this -> viewContent = str_replace("{{".$key."}}", $value, $this -> viewContent);
+				}
+			}
+		}
+
+
+		private function compileTemplates(){
+			do{
+				$this -> compileForEachTemplates();
+				$this -> compileSimpleTemplates();
+			}while($this -> hasCompilables());
+
+		}
+
+		private function hasCompilables(){
+			preg_match_all('/(\{\{>(\s?)(.*)\}\}|\{\{each(\s)(.*)\}\})/', $this -> viewContent, $matches);
+			return !empty($matches[0]);
+		}
+
+		private function compileSimpleTemplates(){
+			preg_match_all('/\{\{>(\s?)(.*)\}\}/', $this -> viewContent, $matches);
+			if(!empty($matches)){
+				foreach($matches[0] as $match){
+					$matchName = trim(str_replace(array("{{>", "}}"), array("", ""), $match));
+					$templateContent = file_get_contents("templates/" . $matchName . ".html");
+
+					if($this -> data != null){
+						if(array_key_exists ($matchName, $this -> data)){
+							foreach($this -> data[$matchName] as $key => $value){
+								$templateContent = str_replace("{{".$key."}}", $value, $templateContent);
+							}
+						}
+					}
+
+					$this -> viewContent = str_replace($match, $templateContent, $this -> viewContent);
+				}
+			}
+		}
+
+
+		private function compileForEachTemplates(){
+			preg_match_all('/\{\{each(\s)(.*)\}\}/', $this -> viewContent, $matches);
+
+			if(!empty($matches)){
+				foreach($matches[0] as $match){
+
+					$expression = explode(" ", trim(str_replace(array("{{each ", "}}"), array("", ""), $match)));
+					$viewData = $expression[0];
+					$view = file_get_contents("templates/" . $expression[1] . ".html");
+					$compiledView = "";
+
+					foreach($this -> data[$viewData] as $key => $value){
+						$temp = $view;
+
+						foreach($value as $valueKey => $simpleValue){
+							$temp = str_replace("{{".$valueKey."}}", $simpleValue, $temp);
+						}
+
+						$compiledView .= $temp;
+					}
+
+					$this -> viewContent = str_replace($match, $compiledView, $this -> viewContent);
+				}
+			}
+
+		}
+
 
 		function __destruct() {
 
